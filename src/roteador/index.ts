5f411cfe798cd62d4007e760d3ecbf016f003d82
Gilvan Almeida <gilvan.almeida@version1.com>
2025-03-11 20:08:02 +0000

    Modifying the state

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,11 +1,19 @@
+import Projetos from "@/views/Projetos.vue";
 import Tarefas from "@/views/Tarefas.vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
-    component: Tarefas
-}]
+const rotas: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Tarefas',
+        component: Tarefas
+    },
+    {
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
+    },
+]
 
 const roteador = createRouter({
     history: createWebHashHistory(),
